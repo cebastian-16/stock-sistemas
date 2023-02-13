582201a5a66ee81ebd471524aca4e8d99a766b66
cebastian_16 <sebastianponton14@gmail.com>
2023-02-13 16:38:54 -0500

    icons

--- Vista/Menu.php
+++ Vista/Menu.php
@@ -53,11 +53,11 @@
     <div class="navbar-header navbar-right">
       <div class="navbar-brand session">
         <?php echo $_SESSION['user_id'] ?>
-        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-person-circle"
-          viewBox="0 0 16 16">
-          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+          class="bi bi-person-check-fill" viewBox="0 0 16 16">
           <path fill-rule="evenodd"
-            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+            d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+          <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
         </svg>
         <a href="cerrarsession.php">
           <span class="glyphicon glyphicon-off"></span>
